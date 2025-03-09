9ff4a0f96f2ce5a9d5b356ebc35ed22852e62a7e
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-09 11:46:10 -0600

    Up

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from routes import recepciones_bp
+from routes import registrar_rutas
 
 app = Flask(__name__)
 
-# Registrar el blueprint de recepciones
-app.register_blueprint(recepciones_bp)
+# Registrar todas las rutas
+registrar_rutas(app)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
